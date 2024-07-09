fc9c1d2c9a0a87649c79fe0b6c14417b9f1ee166
Gilad F <freidkin.gilad@gmail.com>
2024-07-10 01:14:09 +0300

    minor changes and bugfixes

--- src/propogate_simd.hpp
+++ src/propogate_simd.hpp
@@ -38,20 +38,20 @@ inline signed char mm256_extract_epi8var(__m256i val, int index)
 inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
     constexpr int SIMD_SIZE = 8;
-    constexpr int SIMD_VALS_MASK = 0x11111111;
-    const __m256i NEG_ONE = _mm256_set1_epi8(-1);
+    constexpr __mmask32 SIMD_VALS_MASK = 0x11111111;
+    static const __m256i NEG_ONE = _mm256_set1_epi8(-1);
 
     while (k <= end - SIMD_SIZE)
     {
         // Load indices
-        __m256i indices = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(k));\
+        __m256i indices = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(k));
 
         // Gather (32-bit) values
         // Note that the gathered data is in the form [r---] [r---] [r---] ... where r is actual byte and the rest is junk
         __m256i values = _mm256_i32gather_epi32(reinterpret_cast<const int*>(vals), indices, 1);
 
         // check for which values hold v > -1 (v >= 0 since they are int)
-        int cmp_mask = _mm256_movemask_epi8(_mm256_cmpgt_epi8(values, NEG_ONE));
+        __mmask32 cmp_mask = _mm256_movemask_epi8(_mm256_cmpgt_epi8(values, NEG_ONE));
 
         // zero entries that are not valid (remember that we loaded also junk data)
         cmp_mask = cmp_mask & SIMD_VALS_MASK;
@@ -62,7 +62,7 @@ inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
             return prop_result(k + i / 4, mm256_extract_epi8var(values, i));
         }
 
-        k += 8;
+        k += SIMD_SIZE;
     }
 
     // Handle remaining elements
@@ -91,20 +91,20 @@ inline signed char mm512_extract_epi8var(__m512i val, unsigned int index)
 inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
     constexpr int SIMD_SIZE = 16;
-    constexpr unsigned long long SIMD_VALS_MASK = 0x1111111111111111ULL;
-    const __m512i NEG_ONE = _mm512_set1_epi8(-1);
+    constexpr __mmask64 SIMD_VALS_MASK = 0x1111111111111111ULL;
+    static const __m512i NEG_ONE = _mm512_set1_epi8(-1);
 
     while (k <= end - SIMD_SIZE)
     {
         // Load indices
-        __m512i indices = _mm512_loadu_si512(k);\
+        __m512i indices = _mm512_loadu_si512(k);
 
         // Gather (32-bit) values
         // Note that the gathered data is in the form [r---] [r---] [r---] ... where r is actual byte and the rest is junk
         __m512i values = _mm512_i32gather_epi32(indices, vals, 1);
 
         // check for which values hold v > -1 (v >= 0 since they are int)
-        unsigned long long cmp_mask = _mm512_cmpgt_epi8_mask(values, NEG_ONE);
+        __mmask64 cmp_mask = _mm512_cmpgt_epi8_mask(values, NEG_ONE);
 
         // zero entries that are not valid (remember that we loaded also junk data)
         cmp_mask = cmp_mask & SIMD_VALS_MASK;
@@ -115,7 +115,7 @@ inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
             return prop_result(k + i / 4, mm512_extract_epi8var(values, i));
         }
 
-        k += 8;
+        k += SIMD_SIZE;
     }
 
     // Handle remaining elements
