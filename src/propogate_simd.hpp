3b02489c253907dda082b4cb3ce009b057a10426
Gilad F <freidkin.gilad@gmail.com>
2024-07-10 16:02:04 +0300

    .

--- src/propogate_simd.hpp
+++ src/propogate_simd.hpp
@@ -27,33 +27,33 @@ inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
     return prop_result(k, v);
 }
 
-#elif defined(USE_AVX128)
+#elif defined(USE_AVX512)
 
-// SSE2 INTRINSICS
+// AVX-512 INTRINSICS
 inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
-    constexpr int SIMD_SIZE = 4;
-    constexpr __mmask16 SIMD_VALS_MASK = static_cast<unsigned short>(0x1111);
-    static const __m128i NEG_ONE = _mm128_set1_epi8(-1);
+    constexpr int SIMD_SIZE = 16;
+    constexpr __mmask64 SIMD_VALS_MASK = 0x1111111111111111ULL;
+    static const __m512i NEG_ONE = _mm512_set1_epi8(-1);
 
     while (k <= end - SIMD_SIZE)
     {
         // Load indices
-        __m128i  indices = _mm_loadu_si128(reinterpret_cast<const __m128i*>(k));
+        __m512i indices = _mm512_loadu_si512(k);
 
         // Gather (32-bit) values
         // Note that the gathered data is in the form [r---] [r---] [r---] ... where r is actual byte and the rest is junk
-        __m128i values = _mm_i32gather_epi32(reinterpret_cast<const int*>(vals), indices, 1);
+        __m512i values = _mm512_i32gather_epi32(indices, vals, 1);
 
         // check for which values hold v > -1 (v >= 0 since they are int)
-        __mmask16 cmp_mask = _mm_movemask_epi8(_mm_cmpgt_epi8(values, NEG_ONE));
+        __mmask64 cmp_mask = _mm512_cmpgt_epi8_mask(values, NEG_ONE);
 
         // zero entries that are not valid (remember that we loaded also junk data)
         cmp_mask = cmp_mask & SIMD_VALS_MASK;
 
         if (cmp_mask != 0)
         {
-            int i = __builtin_ctz(cmp_mask);
+            int i = __builtin_ctzll(cmp_mask);
             int* k_new = k + i / 4;
             return prop_result(k_new, vals[*k_new]);
         }
@@ -115,33 +115,33 @@ inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
     return prop_result(k, v);
 }
 
-#elif defined(USE_AVX512)
+#elif defined(USE_AVX128)
 
-// AVX-512 INTRINSICS
+// SSE2 INTRINSICS
 inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
-    constexpr int SIMD_SIZE = 16;
-    constexpr __mmask64 SIMD_VALS_MASK = 0x1111111111111111ULL;
-    static const __m512i NEG_ONE = _mm512_set1_epi8(-1);
+    constexpr int SIMD_SIZE = 4;
+    constexpr __mmask16 SIMD_VALS_MASK = static_cast<unsigned short>(0x1111);
+    static const __m128i NEG_ONE = _mm_set1_epi8(-1);
 
     while (k <= end - SIMD_SIZE)
     {
         // Load indices
-        __m512i indices = _mm512_loadu_si512(k);
+        __m128i  indices = _mm_loadu_si128(reinterpret_cast<const __m128i*>(k));
 
         // Gather (32-bit) values
         // Note that the gathered data is in the form [r---] [r---] [r---] ... where r is actual byte and the rest is junk
-        __m512i values = _mm512_i32gather_epi32(indices, vals, 1);
+        __m128i values = _mm_i32gather_epi32(reinterpret_cast<const int*>(vals), indices, 1);
 
         // check for which values hold v > -1 (v >= 0 since they are int)
-        __mmask64 cmp_mask = _mm512_cmpgt_epi8_mask(values, NEG_ONE);
+        __mmask16 cmp_mask = _mm_movemask_epi8(_mm_cmpgt_epi8(values, NEG_ONE));
 
         // zero entries that are not valid (remember that we loaded also junk data)
         cmp_mask = cmp_mask & SIMD_VALS_MASK;
 
         if (cmp_mask != 0)
         {
-            int i = __builtin_ctzll(cmp_mask);
+            int i = __builtin_ctz(cmp_mask);
             int* k_new = k + i / 4;
             return prop_result(k_new, vals[*k_new]);
         }
