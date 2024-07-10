7899e920f929e0c119299b7c86e84a2f4f526696
Gilad F <freidkin.gilad@gmail.com>
2024-07-10 13:04:19 +0300

    .

--- src/propogate_simd.hpp
+++ src/propogate_simd.hpp
@@ -2,6 +2,8 @@
 
 // Uncomment the following line to use AVX-512 instructions
 //#define USE_AVX512
+//#define USE_AVX256
+#define USE_AVX128
 
 struct prop_result
 {
@@ -11,7 +13,11 @@ struct prop_result
     prop_result(int* k, signed char v) : k(k), v(v) { }
 };
 
-inline prop_result prop_vanilla(int* k, const int* end, const signed char* vals)
+# if not defined(USE_AVX256) && not defined(USE_AVX512) && not defined(USE_AVX128)
+
+// TODO: COUNT STATISTIC OF THE LOCATION OF THE NNF LITERAL
+// TODO: TRY CORRELATE THE LOCATION OF THE NNF LIT WITH THE FORMULA STRUCTURE SOMEHOW.
+inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
     signed char v = -1;
 
@@ -21,21 +27,53 @@ inline prop_result prop_vanilla(int* k, const int* end, const signed char* vals)
     return prop_result(k, v);
 }
 
-# ifndef USE_AVX512
+#elif defined(USE_AVX128)
 
-union mm256_indexer
+// TODO: FINISH IMPLEMENTING
+inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
-    __m256i m256;
-    signed char array[32];
-};
+    constexpr int SIMD_SIZE = 4;
+    constexpr __mmask16 SIMD_VALS_MASK = static_cast<unsigned short>(0x1111);
+    static const __m128i NEG_ONE = _mm128_set1_epi8(-1);
 
-inline signed char mm256_extract_epi8var(__m256i val, int index)
-{
-    mm256_indexer tmp;
-    tmp.m256 = val;
-    return tmp.array[index];
+    while (k <= end - SIMD_SIZE)
+    {
+        // Load indices
+        __m256i indices = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(k));
+
+        // Gather (32-bit) values
+        // Note that the gathered data is in the form [r---] [r---] [r---] ... where r is actual byte and the rest is junk
+        __m256i values = _mm256_i32gather_epi32(reinterpret_cast<const int*>(vals), indices, 1);
+
+        // check for which values hold v > -1 (v >= 0 since they are int)
+        __mmask32 cmp_mask = _mm256_movemask_epi8(_mm256_cmpgt_epi8(values, NEG_ONE));
+
+        // zero entries that are not valid (remember that we loaded also junk data)
+        cmp_mask = cmp_mask & SIMD_VALS_MASK;
+
+        if (cmp_mask != 0)
+        {
+            int i = __builtin_ctz(cmp_mask);
+            int* k_new = k + i / 4;
+            return prop_result(k_new, vals[*k_new]);
+        }
+
+        k += SIMD_SIZE;
+    }
+
+    // Handle remaining elements
+    signed char v = -1;
+
+    while (k != end && (v = vals[*k]) < 0)
+        k++;
+
+    return prop_result(k, v);
 }
 
+# elif defined(USE_AVX256)
+
+// TODO: COUNT STATISTICS HOW MANY OF THE CALLS TO PROP_SIMD END UP IN THE SIMD LOOP
+// TODO: COUNT STATISTICS OF HOW MANY ITERS THE SIMD LOOP PERFORMS 
 inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
     constexpr int SIMD_SIZE = 8;
@@ -62,7 +100,6 @@ inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
             int i = __builtin_ctz(cmp_mask);
             int* k_new = k + i / 4;
             return prop_result(k_new, vals[*k_new]);
-            //return prop_result(k + i / 4, mm256_extract_epi8var(values, i));
         }
 
         k += SIMD_SIZE;
@@ -77,20 +114,7 @@ inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
     return prop_result(k, v);
 }
 
-#else
-
-union mm512_indexer
-{
-    __m512i m256;
-    signed char array[64];
-};
-
-inline signed char mm512_extract_epi8var(__m512i val, unsigned int index)
-{
-    mm512_indexer tmp;
-    tmp.m256 = val;
-    return tmp.array[index];
-}
+#elif defined(USE_AVX512)
 
 inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
 {
@@ -116,7 +140,8 @@ inline prop_result prop_simd(int* k, const int* end, const signed char* vals)
         if (cmp_mask != 0)
         {
             int i = __builtin_ctzll(cmp_mask);
-            return prop_result(k + i / 4, mm512_extract_epi8var(values, i));
+            int* k_new = k + i / 4;
+            return prop_result(k_new, vals[*k_new]);
         }
 
         k += SIMD_SIZE;
