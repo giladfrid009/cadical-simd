edb91f4b56d5c6b26016dcf32bf8db0d37f69b80
Gilad F <freidkin.gilad@gmail.com>
2024-09-25 13:02:49 +0300

    .

--- src/propagate_simd.cpp
+++ src/propagate_simd.cpp
@@ -6,8 +6,8 @@ namespace CaDiCaL
     // Uncomment one of the following to enable SIMD propagation
     #define UNROLL
     //#define USE_AVX512
-    #define USE_AVX256
-    //#define USE_AVX128
+    //#define USE_AVX256
+    #define USE_AVX128
 
 # if not defined(USE_AVX256) && not defined(USE_AVX512) && not defined(USE_AVX128)
 
